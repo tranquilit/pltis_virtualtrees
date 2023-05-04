2d6495d9e1e5fdda6ba47971638b886d4f7dce8c
Marcos Douglas B. Santos <msantos@tranquil.it>
2023-05-04 06:45:54 -0300

    restore all files in lowercase again

---
+++ registervirtualtreeview.pas
@@ -0,0 +1,38 @@
+unit RegisterVirtualTreeView; 
+
+{$Mode ObjFpc}
+{$H+}
+
+interface
+
+procedure Register;
+
+implementation
+
+{$R ideicons.res}
+
+
+uses
+  Classes, SysUtils, LResources, LazarusPackageIntf,
+  VirtualTrees, VTHeaderPopup, VTIDEEditors, ComponentEditors;
+
+
+procedure RegisterUnitVirtualTrees;
+begin
+  RegisterComponents('Virtual Controls', [TVirtualDrawTree, TVirtualStringTree]);
+end;  
+
+procedure RegisterUnitVTHeaderPopup;
+begin
+  RegisterComponents('Virtual Controls', [TVTHeaderPopupMenu]);
+end;
+
+procedure Register;
+
+begin
+  RegisterComponentEditor([TVirtualDrawTree, TVirtualStringTree], TVirtualTreeEditor);
+  RegisterUnit('VirtualTrees', @RegisterUnitVirtualTrees);
+  RegisterUnit('VTHeaderPopup', @RegisterUnitVTHeaderPopup);
+end;
+
+end.
